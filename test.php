4cfd2322a00bee3f8ebaea181a845cfec932ac21
tonci <teodor.p.nikolov@gmail.com>
2015-04-14 11:24:01 +0300

    test composer library

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+require 'vendor/autoload.php';
+
+use SimpleORM\Product;
+
+$product = new Product;
+// $product->product_name = 'New mega cool product';
+// $product->id = 12.5;
+// $product->product_name = 'qwertyuiop qwertyuiop qwertyuiop qwertyuiop qwertyuiop';
+// $product->product_discount = 'asd';
+// $product->save();
+
+print_r($product->findAll());
